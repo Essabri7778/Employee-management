4fa6a07378506f4f52f1f24b7303b814fd7a1c21
Benmamoun Nassim <benmamounnassim@outlook.fr>
2023-05-20 19:34:12 +0100

    modifying DB.php

--- database/DB.php
+++ database/DB.php
@@ -1,7 +1,7 @@
 <?php
 class DB{
     static public function connect(){
-        $pdo = new PDO('mysql:host=localhost;dbname=gestion-employes', 'root', 'root');
+        $pdo = new PDO('mysql:host=localhost;dbname=gestion-employes', 'root', '');
         $pdo->exec("set names utf8");
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
