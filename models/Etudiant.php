2e3e9bff0bab4bf92a5619e0e6a35a7e749879bf
Essabri Fatima zahrae <essabrifatimazahrae2001@gmail.com>
2023-05-20 22:36:37 +0100

    adding CRUD methods to Etudiant model

---
+++ models/Etudiant.php
@@ -0,0 +1,88 @@
+<?php
+
+class Etudiant {
+
+    //Récuperer tous les étudiants
+    static public function getAllEtudiants(){
+        $stmt = DB::connect()->prepare('SELECT * FROM etudiant');
+        $stmt->execute();
+        $etudiants = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return json_encode($etudiants);
+        $stmt->close();
+        $stmt = null;
+    }
+
+    //Récuperer un étudiant par son id
+    static public function getEtudiant($id){
+        try{
+            $stmt = DB::connect()->prepare('SELECT * FROM etudiant WHERE id=?');
+            $stmt->execute([$id]);
+            $etudiant = $stmt->fetch(PDO::FETCH_ASSOC);
+            return json_encode($etudiant);
+        }catch(PDOException $e){
+            echo 'Error'. $e->getMessage();
+        }
+    }
+
+    //ajouter un étudiant
+    static public function addEtudiant($data){
+        $stmt = DB::connect()->prepare('INSERT INTO etudiant (nom, prenom, adresse, email, telephone, mot_de_passe) VALUES (:nom,:prenom,:adresse,:email,:telephone,:mot_de_passe)');
+        $stmt->bindParam(':nom',$data['nom']);
+        $stmt->bindParam(':prenom',$data['prenom']);
+        $stmt->bindParam(':matricule',$data['adresse']);
+        $stmt->bindParam(':depart',$data['email']);
+        $stmt->bindParam(':post',$data['telephone']);
+        $stmt->bindParam(':date_emb',$data['mot_de_passe']);
+        
+        if ($stmt->execute()){
+            return 'ok';
+        }else{
+            return 'error';
+        }
+        $stmt->close();
+        $stmt = null;
+    }
+
+    //mettre à jour un étudiant
+    static public function updateEtudiant($data){
+        $stmt = DB::connect()->prepare('UPDATE etudiant SET nom= :nom, prenom= :prenom, adresse= :adresse, email= :email, telephone= :telephone, mot_de_passe= :mot_de_passe WHERE id= :id');
+        $stmt->bindParam(':nom',$data['nom']);
+        $stmt->bindParam(':prenom',$data['prenom']);
+        $stmt->bindParam(':matricule',$data['adresse']);
+        $stmt->bindParam(':depart',$data['email']);
+        $stmt->bindParam(':post',$data['telephone']);
+        $stmt->bindParam(':date_emb',$data['mot_de_passe']);
+        $stmt->bindParam(':id',$data['id']);
+        if ($stmt->execute()){
+            return 'ok';
+        }else{
+            return 'error';
+        }
+        $stmt->close();
+        $stmt = null;
+    }
+
+    //Supprimer un étudiant
+    static public function deleteEtudiant($id){
+        $stmt = DB::connect()->prepare('DELETE FROM etudiant WHERE id=?');
+        if ($stmt->execute([$id])){
+            return 'ok';
+        }else{
+            return 'error';
+        }
+        $stmt->close();
+        $stmt = null;
+    }
+
+    //Trouver un étudiant par son nom ou prenom
+    // static public function findEtudiant($data){
+    //     $search = $data['search'];
+    //     try{
+    //         $stmt = DB::connect()->prepare('SELECT * FROM etudiant WHERE nom LIKE ? OR prenom LIKE ?');
+    //         $stmt->execute(['%'.$search.'%','%'.$search.'%']);
+    //         return $stmt->fetchAll();
+    //     }catch(PDOException $e){
+    //         echo 'Error'. $e->getMessage();
+    //     }
+    // }
+}
