963fb6d028e4a2475be878f08d760024548b36d4
Benmamoun Nassim <benmamounnassim@outlook.fr>
2023-05-23 20:21:54 +0100

    etudiant still needs afficher mes modules

--- scripts/afficheEtdEval.js
+++ scripts/afficheEtdEval.js
@@ -8,7 +8,6 @@ function populateList() {
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
       let list = xhr.responseText;
-      console.log(JSON.parse(list));
       afficherEval(JSON.parse(list));
     }
   };
@@ -35,4 +34,26 @@ function afficherEval(evaluations) {
     `;
     tbody.insertAdjacentHTML("beforeend", neweval);
   }
+}
+
+// search form
+
+let searchForm = document.getElementById("formulaire");
+
+searchForm.addEventListener("submit", searchEvaluation);
+
+function searchEvaluation(e) {
+  e.preventDefault();
+  let xhr = getXhr();
+  xhr.open("POST", "../../controllers/EvaluationController.php", true);
+  xhr.addEventListener("readystatechange", function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let res = xhr.responseText;
+      afficherEval(JSON.parse(res));
+    }
+  });
+
+  let data = new FormData(searchForm);
+  data.append("action", "chercherMesEval");
+  xhr.send(data);
 }
--- views/etudiant/afficherEtdNotes.php
+++ views/etudiant/afficherEtdNotes.php
@@ -2,46 +2,49 @@
 
 <div class="container" style="padding: 20px;">
 
-  <div class="container-fluid bg-primary text-white">
-    <div class="text-center">
-      <h1 class="display-2 fw-bold" style="background-color: rgba(0, 0, 0, 0.7); padding: 20px;">
-        Liste de Mes Notes
-      </h1>
+    <div class="container-fluid bg-primary text-white">
+        <div class="text-center">
+            <h1 class="display-2 fw-bold" style="background-color: rgba(0, 0, 0, 0.7); padding: 20px;">
+                Liste de Mes Notes
+            </h1>
+        </div>
     </div>
-  </div>
+    <form id="formulaire">
+        <div class="form-group row mb-3">
+            <div class="input-group">
+                <div class="form-outline flex-grow-1">
+                    <input type="search" id="searchBar" name="search" class="form-control" placeholder="Chercher" />
+                </div>
+                <button type="submit" class="btn btn-primary">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </div>
+    </form>
 
-  <div class="form-group row mb-3">
-    <div class="input-group">
-      <div class="form-outline flex-grow-1">
-        <input type="search" id="searchBar" class="form-control" placeholder="Chercher" />
-      </div>
-      <button type="button" class="btn btn-primary">
-        <i class="fas fa-search"></i>
-      </button>
-    </div>
-  </div>
-
-  <table id="personnes" class="table table-bordered border-dark">
-    <thead>
-      <tr>
-        <th scope="col"><i class="fas fa-bookmark"></i> Evaluation</th>
-        <th scope="col"><i class="fas fa-book"></i> Module</th>
-        <th scope="col"><i class="fas fa-chart-bar"></i> Valeure</th>
-      </tr>
-    </thead>
-    <tbody id="listStudent">
-      <tr>
-        <td>Type</td>
-        <td>Module</td>
-        <td>20</td>
-      </tr>
-    </tbody>
-  </table>
+    <table id="personnes" class="table table-bordered border-dark">
+        <thead>
+            <tr>
+                <th scope="col"><i class="fas fa-bookmark"></i> Evaluation</th>
+                <th scope="col"><i class="fas fa-book"></i> Module</th>
+                <th scope="col"><i class="fas fa-chart-bar"></i> Valeur</th>
+            </tr>
+        </thead>
+        <tbody id="listNotes">
+            <tr>
+                <td>Type</td>
+                <td>Module</td>
+                <td>20</td>
+            </tr>
+        </tbody>
+    </table>
 </div>
 
 
-<script src="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/js/all.min.js" crossorigin="anonymous">
+</script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="../../scripts/afficherEtdNotes.js" type="module"></script>
 </body>
 
 </html>
